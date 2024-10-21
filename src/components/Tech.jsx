0e80cbba7077c34e574af6b694caa9b3641c6cd7
TheKorosenai <akshatjain2411@gmail.com>
2024-10-21 07:01:05 +0530

    made animations smoother

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,11 +1,17 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
-
+import { motion } from 'framer-motion';
+import { textVariant } from '../utils/motion';
+import { styles } from '../styles';
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
+    <div className="flex flex-col flex-wrap gap-10">
+      <motion.div variants={textVariant()}>
+        <h2 className={styles.sectionHeadText}>Skills.</h2>
+      </motion.div>
+      <div className="flex flex-row flex-wrap justify-center gap-10 mt-6">
       { technologies.map((technology) => (
         <div className="flex flex-col items-center w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon}/>
@@ -13,7 +19,7 @@ const Tech = () => {
             {technology.name}
           </div>
         </div>
-      ))}
+      ))}</div>
     </div>
   )
 }
